4150490d025c7f286230b922c9319949ac69fdfb
eihcol <88992983+eihcol@users.noreply.github.com>
2021-09-06 11:55:57 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ function setup() {
   createCanvas(400, 400); 
   
   btn_red = createButton("RED");
-  btn_red.postion(100,50);
-  btn_red.mousePressed(red_bg);
+  btn_red.position(100,50);
+  
 
   btn_green = createButton("GREEN");
   btn_green.position(250,50);
-  btn_green.mousePressed(green_bg);
+  
 
   
 }
@@ -22,21 +22,22 @@ function setup() {
 
 function draw() {
   background(r,g,b);
-  drawSprites();
-function red_bg(){
-
-
   
-    r = 255;
-    g = 0;
-    b = 0;
+  btn_red.mousePressed(red_bg);
+  btn_green.mousePressed(green_bg);
+
+  //drawSprites();
+
+}
+
+  function red_bg(){
+  r = 255;
+  g = 0;
+  b = 0;
   }
-function green_bg(){
+  function green_bg(){
   r = 0;
   g = 255;
   b = 0;
 }
-function drawSprites()
-  }
-
 
